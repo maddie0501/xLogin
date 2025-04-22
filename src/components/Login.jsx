ac29ac4f161db6328e081d17dc872c0a6ebdee43
maddie0501 <madhurikavatekar01@gmail.com>
2025-04-22 15:15:41 +0530

    login functionality commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [issubmit, setIssubmit] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username === "user" && password === "password") {
+      setError(false);
+      setIssubmit(true);
+    } else {
+      // console.log("login");
+      setIssubmit(false);
+      setError(true);
+    }
+  };
+
+  return (
+    <div>
+      {!issubmit ? (
+        <>
+          <h1>Login Page</h1>
+
+          {error && <p>Invalid username or password</p>}
+
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input
+                type="text"
+                name="username"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1>Login Page</h1>
+
+          <h4>Welcome, user!</h4>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Login;
